13a3b0725d93e4fb6d102e0c1706a5391dfe504c
moskowitza <moskowitzaaron@mac.com>
2018-05-02 18:33:09 -0400

    Trying to get this Chosen plug in to work

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 // You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
-var json=
+var friendArray= [
 {
   "name":"Ahmed",
   "photo":"https://c1-zingpopculture.eb-cdn.com.au/merchandising/images/packshots/79fa32c32cd047679823f9451c8313d4_Original.png",
@@ -16,11 +16,32 @@ var json=
       4,
       1
     ]
-}
+},
+{
+    "name":"Dr. Teeth",
+    "photo":"https://c1-zingpopculture.eb-cdn.com.au/merchandising/images/packshots/79fa32c32cd047679823f9451c8313d4_Original.png",
+    "scores":[
+        3,
+        4,
+        5,
+        7,
+        8,
+        1,
+        3,
+        4,
+        5,
+        8
+      ]
+  },
+
+];
+module.exports = friendArray;
 
 // 6. Determine the user's most compatible friend using the following as a guide:
 
 //    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+
+
 //    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
 //      * Example: 
 //        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
