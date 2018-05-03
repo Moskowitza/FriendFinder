7f50e7784a87b47e9fee010a510536c565acb4b6
moskowitza <moskowitzaaron@mac.com>
2018-05-03 11:37:05 -0400

    working on logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,47 +1,82 @@
 // You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
-var friendArray= [
-{
-  "name":"Ahmed",
-  "photo":"https://c1-zingpopculture.eb-cdn.com.au/merchandising/images/packshots/79fa32c32cd047679823f9451c8313d4_Original.png",
-  "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-},
-{
-    "name":"Dr. Teeth",
-    "photo":"https://c1-zingpopculture.eb-cdn.com.au/merchandising/images/packshots/79fa32c32cd047679823f9451c8313d4_Original.png",
-    "scores":[
+var friendArray = [
+    {
+        "name": "Ahmed",
+        "photo": "https://c1-zingpopculture.eb-cdn.com.au/merchandising/images/packshots/79fa32c32cd047679823f9451c8313d4_Original.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ],
+    },
+    {
+        "name": "Dr. Teeth",
+        "photo": "https://c1-zingpopculture.eb-cdn.com.au/merchandising/images/packshots/79fa32c32cd047679823f9451c8313d4_Original.png",
+        "scores": [
+            3,
+            4,
+            5,
+            7,
+            8,
+            1,
+            3,
+            4,
+            5,
+            8
+        ]
+    },
+];
+var newFriend = {
+    "name": "SpongeBob",
+    "photo": "https://c1-zingpopculture.eb-cdn.com.au/merchandising/images/packshots/79fa32c32cd047679823f9451c8313d4_Original.png",
+    "scores": [
         3,
         4,
         5,
         7,
         8,
-        1,
+        9,
         3,
         4,
-        5,
+        2,
         8
-      ]
-  },
+    ]
+}
 
-];
 module.exports = friendArray;
 
+// Function to add scores for each array
+var friendScores = [];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+for (i = 0; i < friendArray.length; i++) {
+    var numbers = friendArray[i].scores;
+    console.log(numbers.reduce(reducer))
+    //push them into a new array
+    friendScores.push(numbers.reduce(reducer))
+}
+console.log(friendScores)
+// calculate NewFriend Score
+var newFriendScore = newFriend.scores.reduce(reducer);
+console.log(newFriendScore);
+
+var differences = [];
+for (i = 0; i < friendScores.length; i++) {
+    // get absolute value
+    var dif=Math.abs(friendScores[i] - newFriendScore)
+    differences.push(dif);
+}
+console.log(differences)
+
 // 6. Determine the user's most compatible friend using the following as a guide:
-
 //    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-
-
 //    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
 //      * Example: 
 //        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
