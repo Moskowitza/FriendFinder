3de8b5bca7d91f445a5ca747a12ec6aaf7d3077b
moskowitza <moskowitzaaron@mac.com>
2018-05-03 12:17:05 -0400

    finding better solutions along the way

--- app/data/friends.js
+++ app/data/friends.js
@@ -24,13 +24,29 @@ var friendArray = [
             3,
             4,
             5,
-            7,
-            8,
+            1,
+            1,
             1,
             3,
             4,
             5,
-            8
+            1
+        ]
+    },
+    {
+        "name": "Sandman",
+        "photo": "https://c1-zingpopculture.eb-cdn.com.au/merchandising/images/packshots/79fa32c32cd047679823f9451c8313d4_Original.png",
+        "scores": [
+            3,
+            3,
+            5,
+            1,
+            3,
+            5,
+            3,
+            4,
+            5,
+            2
         ]
     },
 ];
@@ -38,16 +54,16 @@ var newFriend = {
     "name": "SpongeBob",
     "photo": "https://c1-zingpopculture.eb-cdn.com.au/merchandising/images/packshots/79fa32c32cd047679823f9451c8313d4_Original.png",
     "scores": [
-        3,
+        5,
         4,
         5,
-        7,
-        8,
-        9,
+        5,
+        1,
         3,
-        4,
+        5,
+        5,
         2,
-        8
+        5
     ]
 }
 
@@ -58,23 +74,41 @@ var friendScores = [];
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 for (i = 0; i < friendArray.length; i++) {
     var numbers = friendArray[i].scores;
-    console.log(numbers.reduce(reducer))
     //push them into a new array
     friendScores.push(numbers.reduce(reducer))
 }
-console.log(friendScores)
-// calculate NewFriend Score
+console.log("Array of Friend Scores "+friendScores)
+// calculate USER'S Score
 var newFriendScore = newFriend.scores.reduce(reducer);
-console.log(newFriendScore);
+// console.log(newFriendScore);
 
-var differences = [];
-for (i = 0; i < friendScores.length; i++) {
-    // get absolute value
-    var dif=Math.abs(friendScores[i] - newFriendScore)
-    differences.push(dif);
+//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on. 
+// var differences = [];
+// for (i = 0; i < friendScores.length; i++) {
+//     // get absolute value
+//     var dif=Math.abs(friendScores[i] - newFriendScore)
+//     differences.push(dif);
+// }
+
+function closest (num, arr) {
+    var curr = arr[0];
+    var diff = Math.abs (num - curr);
+    for (var val = 0; val < arr.length; val++) {
+        var newdiff = Math.abs (num - arr[val]);
+        if (newdiff < diff) {
+            diff = newdiff;
+            curr = arr[val];
+        }
+    }
+    return curr;
 }
-console.log(differences)
 
+console.log(closest(newFriendScore,friendScores));
+matchVal=closest(newFriendScore,friendScores);
+//use index of to get the position of the friend match
+index=friendScores.indexOf(matchVal);
+matchedName=friendArray[index].name;
+console.log(matchedName);
 // 6. Determine the user's most compatible friend using the following as a guide:
 //    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
 //    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
